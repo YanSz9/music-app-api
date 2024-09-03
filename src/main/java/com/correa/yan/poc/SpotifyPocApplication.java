044ef9f928fc3fc58a5400d10c20d3e1ea742992
Yan Corrêa <yansouza009@gmail.com>
2024-09-03 19:19:02 -0300

    feta: initial commit

---
+++ src/main/java/com/correa/yan/poc/SpotifyPocApplication.java
@@ -0,0 +1,13 @@
+package com.correa.yan.poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifyPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotifyPocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/correa/yan/poc/SpotifyPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.correa.yan.poc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifyPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
